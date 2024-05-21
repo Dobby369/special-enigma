b454e4a36b7d70da0687da6da7ef4dba7ad2c6af
Tomogochi <malkova.ol939@gmail.com>
2024-05-21 22:18:20 +0300

    session 1

---
+++ WpfApp3/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Singleton
+    {
+        public static readonly RemontKOEntities DB = new RemontKOEntities();
+    }
+}
---
+++ WpfApp3/pages/Services.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Services.xaml
+    /// </summary>
+    public partial class Services : Page
+    {
+        public Services()
+        {
+            InitializeComponent();
+        }
+
+        private void table_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            MainWindow mai = Window.GetWindow(this) as MainWindow;
+            mai.frame.Navigate(new Services());
+
+            Singleton.DB.Servic.ToList();
+            table.ItemsSource = Singleton.DB.Servic.Local;
+        }
+    }
+}
